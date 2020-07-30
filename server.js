a0063e68e6303d173f0c37d6e79adba448be29e9
Joyce Querubino <joycequerubino5@gmail.com>
2020-07-30 16:50:02 -0300

    adicionando dados dinâmicos na home

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ server.use(express.static('public'))
 server.set("view engine" , "njk")
 
 nunjucks.configure("views" , {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 //Criando porta
@@ -23,8 +24,19 @@ server.listen(5000, function() {
 })
 
 // Rota da página principal - home
-server.get("/", function (req , res){
-    return res.render("about")
+server.get("/", function (req , res){   
+    const about = {
+        avatar_url: "https://pbs.twimg.com/profile_images/1271517147349626881/Mf1UjRa0_400x400.jpg", 
+        name: "RocketSeat",
+        role: "Ensinando alunos a Codar!", 
+        description: "Imagine você dominando as mesmas tecnologias adotadas pelos melhores times do mundo, construindo aplicações de alta performance e se destacando entre os maiores programadores.",
+        links: [
+            {name: "GitHub", url: "https://github.com/Rocketseat"}, 
+            {name: "Linkedin", url: "https://linkedin.com/school/rocketseat/?originalSubdomain=br"},
+            {name: "Facebook", url: "https://pt-br.facebook.com/rocketseat/"}
+        ]
+    }
+    return res.render("about", {about})
 })
 
 // Rota da página cursos 
