14041390d95a5584f4ab152a46db584b3145640a
Joyce Querubino <joycequerubino5@gmail.com>
2020-08-16 20:08:39 -0300

    Atualizando

--- server.js
+++ server.js
@@ -44,7 +44,17 @@ server.get("/", function (req , res){
 server.get("/video", function (req , res){
     const id = req.query.id;
 
-    res.send(id)
+    const video = videos.find(function(video){
+        if(video.id == id){
+            return true; 
+        }
+    })
+
+    if(!video){
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", { item: video }) 
 })
 
 
